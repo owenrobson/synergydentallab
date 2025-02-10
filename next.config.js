904269f4091ceb603713ad82b92bfba5cda8d00f
Owen Robson <owen.robson@adi.do>
2025-02-10 16:24:10 +0000

    fix again

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/synergydentallab", // Required for GitLab Pages
-  assetPrefix: "/synergydentallab", // Ensures assets load correctly
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: {
-    unoptimized: true, // Required for static exports
-  },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
